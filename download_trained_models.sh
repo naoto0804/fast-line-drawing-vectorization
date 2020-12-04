686335f543ef2b9ec35e8e5505b73c030a6e2038
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2020-12-04 20:17:35 +0900

    Create download_trained_models.sh

---
+++ download_trained_models.sh
@@ -0,0 +1,6 @@
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/ch_model_final.pth
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/line_model_final.pth
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/baseball_model_final.pth
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/kanji_model_final.pth
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/multi_model_final.pth
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/fast_line_drawing_vectorization/models/cat_model_final.pth
